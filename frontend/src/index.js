4940d688bab74a832ef521cba98751d263db48f3
Sean <Sransonette@gmail.com>
2021-02-16 17:11:00 -0600

    add form to index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log('hi')
-    //fetchPokemons()
-    //mountFormListener()
+    fetchPokemons()
+    formListener()
 
 } )
 
 function fetchPokemons() {
     fetch('http://localhost:3000/api/v1/pokemons')
+    .then(resp => resp.json())
+    .then(pokemons => console.log(pokemons))
+}
+
+function formListener(){
+    const postForm = 
 }
